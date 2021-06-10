b7fefc10463f7ac4d8f7cca4f06abf7948aef255
developer <developer@mediatek.com>
2021-06-10 19:36:07 +0800

    [][Panther/Merlin DEV]
    [Description]
    Add support mtk RFB4 board
    
    [Release-log]
    
    Change-Id: I5de063a0236fc49e27ac0388d9bc0f6cc18c5225
    Reviewed-on: https://gerrit.mediatek.inc/c/openwrt/feeds/mtk_openwrt_feeds/+/4632457

--- target/linux/mediatek/image/mt7986.mk
+++ target/linux/mediatek/image/mt7986.mk
@@ -100,6 +100,32 @@ define Device/mt7986b-mt7975-ax6000-rfb1-snand
 endef
 TARGET_DEVICES += mt7986b-mt7975-ax6000-rfb1-snand
 
+define Device/mt7986b-mt7976-ax6000-rfb4
+  DEVICE_VENDOR := MediaTek
+  DEVICE_MODEL := mt7986b-mt7976-ax6000-rfb4
+  DEVICE_DTS := mt7986b-mt7976-ax6000-rfb4
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+endef
+TARGET_DEVICES += mt7986b-mt7976-ax6000-rfb4
+
+define Device/mt7986b-mt7976-ax6000-rfb4-snand
+  DEVICE_VENDOR := MediaTek
+  DEVICE_MODEL := mt7986b-mt7976-ax6000-rfb4 (SPI-NAND,UBI)
+  DEVICE_DTS := mt7986b-mt7976-ax6000-rfb4
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  SUPPORTED_DEVICES := mediatek,mt7986-rfb-snand
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 65536k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += mt7986b-mt7976-ax6000-rfb4-snand
+
+
 define Device/mediatek_mt7986-fpga
   DEVICE_VENDOR := MediaTek
   DEVICE_MODEL := MTK7986 FPGA
