765b1983512bb1237c9f810a5bfb1ffb3ff3354a
developer <developer@mediatek.com>
2021-06-24 09:18:15 +0800

    [][PATCH 2/2: USB 2.0 USBIF Compliance toolkit]
    [Description]
    Add USB 2.0 USBIF Compliance toolkit
    
    [Release-log]
    N/A
    
    Change-Id: Ia8c81a309dd56280a68bda0b6097c5c53a4e92bd
    Reviewed-on: https://gerrit.mediatek.inc/c/openwrt/feeds/mtk_openwrt_feeds/+/4674249

---
+++ target/linux/mediatek/files-5.4/drivers/usb/host/xhci-mtk-test.h
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2015 MediaTek Inc.
+ * Author:
+ *  Shaocheng.Wang <shaocheng.wang@mediatek.com>
+ *  Chunfeng.Yun <chunfeng.yun@mediatek.com>
+ *
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ */
+
+#ifndef __XHCI_MTK_TEST_H
+#define __XHCI_MTK_TEST_H
+
+int mu3h_hqa_create_attr(struct device *dev);
+void mu3h_hqa_remove_attr(struct device *dev);
+
+#endif /* __XHCI_MTK_TEST_H */
