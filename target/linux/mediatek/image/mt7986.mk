f9a2c030b66be873151844116ced72bda9121e8d
developer <developer@mediatek.com>
2021-09-30 17:18:10 +0800

    [][dts: mt7986: add snfi dts for 2.5Gphy board]
    [Description]
    Add snfi dts for 2.5Gphy board.
    
    [Release-log]
    N/A
    
    Change-Id: Ic26db8a05d819722dbbabe5f60b9c98421451e3f
    Reviewed-on: https://gerrit.mediatek.inc/c/openwrt/feeds/mtk_openwrt_feeds/+/5064130

--- target/linux/mediatek/image/mt7986.mk
+++ target/linux/mediatek/image/mt7986.mk
@@ -206,6 +206,23 @@ define Device/mt7986b-ax6000-2500wan-spim-nand-rfb
 endef
 TARGET_DEVICES += mt7986b-ax6000-2500wan-spim-nand-rfb
 
+define Device/mt7986b-ax6000-2500wan-snfi-nand-rfb
+  DEVICE_VENDOR := MediaTek
+  DEVICE_MODEL := mt7986b-ax6000-2500wan-snfi-nand-rfb (SPI-NAND,UBI)
+  DEVICE_DTS := mt7986b-2500wan-snfi-nand-rfb
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  SUPPORTED_DEVICES := mediatek,mt7986b-2500wan-snfi-nand-rfb
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 65536k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += mt7986b-ax6000-2500wan-snfi-nand-rfb
+
 define Device/mediatek_mt7986-fpga
   DEVICE_VENDOR := MediaTek
   DEVICE_MODEL := MTK7986 FPGA
