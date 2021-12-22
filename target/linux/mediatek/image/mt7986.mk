15d351e4b8c17ce2ef7bc1eb19eab744a4cbc007
developer <developer@mediatek.com>
2021-12-22 19:53:19 +0800

    [][Add non-DSA MT7531 driver support on Panther]
    [Description]
    Add non-DSA MT7531 driver support on Panther for 2.5G LAN/WAN
    peak throughput test without additional 4-bytes special tag
    used by DSA switch driver.
    
    Driver difference:
    DSA: "lan0" to "lan5" network interface for each switch port.
    Non-DSA: "eth0" for all LAN ports, "eth1" for WAN port.
    
    [Release-log]
    N/A
    
    Change-Id: I27a3efd10d1bc1709a4123331266adbf04b2ca7f
    Reviewed-on: https://gerrit.mediatek.inc/c/openwrt/feeds/mtk_openwrt_feeds/+/5422351

--- target/linux/mediatek/image/mt7986.mk
+++ target/linux/mediatek/image/mt7986.mk
@@ -97,6 +97,23 @@ define Device/mt7986a-ax6000-2500wan-spim-nand-rfb
 endef
 TARGET_DEVICES += mt7986a-ax6000-2500wan-spim-nand-rfb
 
+define Device/mt7986a-ax6000-2500wan-gsw-spim-nand-rfb
+  DEVICE_VENDOR := MediaTek
+  DEVICE_MODEL := mt7986a-ax6000-2500wan-gsw-spim-nand-rfb (SPI-NAND,UBI)
+  DEVICE_DTS := mt7986a-2500wan-gsw-spim-nand-rfb
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  SUPPORTED_DEVICES := mediatek,mt7986a-2500wan-gsw-spim-nand-rfb
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 65536k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += mt7986a-ax6000-2500wan-gsw-spim-nand-rfb
+
 define Device/mt7986a-ax8400-2500wan-spim-nand-rfb
   DEVICE_VENDOR := MediaTek
   DEVICE_MODEL := mt7986a-ax8400-2500wan-spim-nand-rfb (SPI-NAND,UBI)
@@ -243,6 +260,23 @@ define Device/mt7986b-ax6000-2500wan-spim-nand-rfb
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += mt7986b-ax6000-2500wan-spim-nand-rfb
+
+define Device/mt7986b-ax6000-2500wan-gsw-spim-nand-rfb
+  DEVICE_VENDOR := MediaTek
+  DEVICE_MODEL := mt7986b-ax6000-2500wan-gsw-spim-nand-rfb (SPI-NAND,UBI)
+  DEVICE_DTS := mt7986b-2500wan-gsw-spim-nand-rfb
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  SUPPORTED_DEVICES := mediatek,mt7986b-2500wan-gsw-spim-nand-rfb
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 65536k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += mt7986b-ax6000-2500wan-gsw-spim-nand-rfb
 
 define Device/mt7986b-ax6000-2500wan-snfi-nand-rfb
   DEVICE_VENDOR := MediaTek
