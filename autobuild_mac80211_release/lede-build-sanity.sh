9448bcd1bab90909ff4631d858e43e7e3b48cb2f
developer <developer@mediatek.com>
2022-06-14 13:19:41 +0800

    [][MAC80211][build][Fix mistypo commands]
    [Description]
    Fix mistypo commands for external release
    
    [Release-log]
    N/A
    
    Change-Id: Ie1df927ec128ed4fe2624666ed437a3e61d67ff3
    Reviewed-on: https://gerrit.mediatek.inc/c/openwrt/feeds/mtk_openwrt_feeds/+/6108868

--- autobuild_mac80211_release/lede-build-sanity.sh
+++ autobuild_mac80211_release/lede-build-sanity.sh
@@ -113,7 +113,7 @@ prepare_mac80211() {
 	cp -fpR ${BUILD_DIR}/./../mac80211_package/package/libs/libnl-tiny ${BUILD_DIR}/package/libs
 
 	rm -rf ${BUILD_DIR}/package/network/utils/iw
-	p -fpR ${BUILD_DIR}/./../mac80211_package/package/network/utils/iw ${BUILD_DIR}/package/network/utils
+	cp -fpR ${BUILD_DIR}/./../mac80211_package/package/network/utils/iw ${BUILD_DIR}/package/network/utils
 
 	rm -rf ${BUILD_DIR}/package/network/utils/iwinfo
 	cp -fpR ${BUILD_DIR}/./../mac80211_package/package/network/utils/iwinfo ${BUILD_DIR}/package/network/utils
@@ -122,7 +122,7 @@ prepare_mac80211() {
 	cp -fpR ${BUILD_DIR}/./../mac80211_package/package/kernel/mac80211 ${BUILD_DIR}/package/kernel
 
 	rm -rf ${BUILD_DIR}/package/firmware/wireless-regdb
-    cp -fpR ${BUILD_DIR}/./../mac80211_package/package/firmware/wireless-regdb ${BUILD_DIR}/package/firmware
+	cp -fpR ${BUILD_DIR}/./../mac80211_package/package/firmware/wireless-regdb ${BUILD_DIR}/package/firmware
 
 	cp -fpR ${BUILD_DIR}/./../mac80211_package/package/kernel/mt76 ${BUILD_DIR}/package/kernel
 
