ba570f00394d5d4f27bcddcc778ff546d681c5ff
developer <developer@mediatek.com>
2023-02-08 15:18:10 +0800

    [][Kernel][mt7988][eth][Add sw padding in Ethernet Tx]
    [Description]
    Add software padding processing in Ethernet Tx path.
    
    It's found that too-short packets would lead to
    switch Tx CRC error, followed by switch output
    queue stuck issue. So Ethernet driver should check
    if the packet is too short and conduct software
    padding when necessary.
    
    If without this patch, switch might encounter output
    queue stuck issue.
    
    [Release-log]
    N/A
    
    
    Change-Id: Ibd94cbf3be0530d2b9ee61477b0362d099c47d8e
    Reviewed-on: https://gerrit.mediatek.inc/c/openwrt/feeds/mtk_openwrt_feeds/+/7109899

--- target/linux/mediatek/files-5.4/drivers/net/ethernet/mediatek/mtk_eth_soc.c
+++ target/linux/mediatek/files-5.4/drivers/net/ethernet/mediatek/mtk_eth_soc.c
@@ -1456,6 +1456,13 @@ static int mtk_tx_map(struct sk_buff *skb, struct net_device *dev,
 	int i, n_desc = 1;
 	int k = 0;
 
+	if (skb->len < 32) {
+		if (skb_put_padto(skb, MTK_MIN_TX_LENGTH))
+			return -ENOMEM;
+
+		txd_info.size = skb_headlen(skb);
+	}
+
 	itxd = ring->next_free;
 	itxd_pdma = qdma_to_pdma(ring, itxd);
 	if (itxd == ring->last_free)
