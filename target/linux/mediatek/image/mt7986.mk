d5853bc31c095ce98ada2e8b6c98c28dc3a54c6b
developer <developer@mediatek.com>
2023-02-23 12:52:33 +0800

    [][[Panther] AX5400 development]
    [Description]
    Add AX5400 support
    
    [Release-log]
    N/A
    
    Change-Id: I28bd30e942f19a34cf0f52a94399aeb485432564
    Reviewed-on: https://gerrit.mediatek.inc/c/openwrt/feeds/mtk_openwrt_feeds/+/7172196

--- target/linux/mediatek/image/mt7986.mk
+++ target/linux/mediatek/image/mt7986.mk
@@ -86,6 +86,19 @@ define Device/mt7986a-ax7800-emmc-rfb
 endef
 TARGET_DEVICES += mt7986a-ax7800-emmc-rfb
 
+define Device/mt7986a-ax5400-emmc-rfb
+  DEVICE_VENDOR := MediaTek
+  DEVICE_MODEL := mt7986a-ax5400-emmc-rfb
+  DEVICE_DTS := mt7986a-emmc-rfb
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  SUPPORTED_DEVICES := mediatek,mt7986a-emmc-rfb
+  DEVICE_PACKAGES := mkf2fs e2fsprogs blkid blockdev losetup kmod-fs-ext4 \
+                     kmod-mmc kmod-fs-f2fs kmod-fs-vfat kmod-nls-cp437 \
+                     kmod-nls-iso8859-1
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += mt7986a-ax5400-emmc-rfb
+
 define Device/mt7986a-ax6000-2500wan-spim-nor-rfb
   DEVICE_VENDOR := MediaTek
   DEVICE_MODEL := mt7986a-ax6000-2500wan-spim-nor-rfb
@@ -185,6 +198,23 @@ define Device/mt7986a-ax7800-spim-nand-rfb
 endef
 TARGET_DEVICES += mt7986a-ax7800-spim-nand-rfb
 
+define Device/mt7986a-ax5400-spim-nand-rfb
+  DEVICE_VENDOR := MediaTek
+  DEVICE_MODEL := mt7986a-ax5400-spim-nand-rfb (SPI-NAND,UBI)
+  DEVICE_DTS := mt7986a-spim-nand-rfb
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  SUPPORTED_DEVICES := mediatek,mt7986a-spim-snand-rfb
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 65536k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += mt7986a-ax5400-spim-nand-rfb
+
 define Device/mt7986a-ax7800-2500wan-spim-nor-rfb
   DEVICE_VENDOR := MediaTek
   DEVICE_MODEL := mt7986a-ax7800-2500wan-spim-nor-rfb
@@ -210,6 +240,23 @@ define Device/mt7986a-ax7800-2500wan-spim-nand-rfb
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
 TARGET_DEVICES += mt7986a-ax7800-2500wan-spim-nand-rfb
+
+define Device/mt7986a-ax5400-2500wan-spim-nand-rfb
+  DEVICE_VENDOR := MediaTek
+  DEVICE_MODEL := mt7986a-ax5400-2500wan-spim-nand-rfb (SPI-NAND,UBI)
+  DEVICE_DTS := mt7986a-2500wan-spim-nand-rfb
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  SUPPORTED_DEVICES := mediatek,mt7986a-2500wan-spim-snand-rfb
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 65536k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += mt7986a-ax5400-2500wan-spim-nand-rfb
 
 define Device/mt7986b-ax6000-spim-nor-rfb
   DEVICE_VENDOR := MediaTek
