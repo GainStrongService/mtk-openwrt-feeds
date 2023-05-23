5c6d59896dddf30e404df7757abf55d8640ee53a
developer <developer@mediatek.com>
2023-05-23 15:16:44 +0800

    [][kernel][mt7988][bsp][Rename MT7988C DTS to the MT7988D]
    [Description]
    Change DTS name of the MT7988C to the MT7988D.
    
    [Release-log]
    N/A
    
    
    Change-Id: Ibf3b87684f224c235133472c8aa5e638b0afcf96
    Reviewed-on: https://gerrit.mediatek.inc/c/openwrt/feeds/mtk_openwrt_feeds/+/7528542

--- target/linux/mediatek/image/mt7988.mk
+++ target/linux/mediatek/image/mt7988.mk
@@ -225,12 +225,12 @@ define Device/mediatek_mt7988a-dsa-10g-sd
 endef
 TARGET_DEVICES += mediatek_mt7988a-dsa-10g-sd
 
-define Device/mediatek_mt7988c-gsw-10g-spim-nand
+define Device/mediatek_mt7988d-gsw-10g-spim-nand
   DEVICE_VENDOR := MediaTek
-  DEVICE_MODEL := mt7988c-gsw-10g-spim-nand
-  DEVICE_DTS := mt7988c-gsw-10g-spim-nand
+  DEVICE_MODEL := mt7988d-gsw-10g-spim-nand
+  DEVICE_DTS := mt7988d-gsw-10g-spim-nand
   DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
-  SUPPORTED_DEVICES := mediatek,mt7988c-gsw-10g-spim-snand
+  SUPPORTED_DEVICES := mediatek,mt7988d-gsw-10g-spim-snand
   UBINIZE_OPTS := -E 5
   BLOCKSIZE := 128k
   PAGESIZE := 2048
@@ -240,14 +240,14 @@ define Device/mediatek_mt7988c-gsw-10g-spim-nand
   IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
-TARGET_DEVICES += mediatek_mt7988c-gsw-10g-spim-nand
+TARGET_DEVICES += mediatek_mt7988d-gsw-10g-spim-nand
 
-define Device/mediatek_mt7988c-gsw-10g-sfp-spim-nand
+define Device/mediatek_mt7988d-gsw-10g-sfp-spim-nand
   DEVICE_VENDOR := MediaTek
-  DEVICE_MODEL := mt7988c-gsw-10g-sfp-spim-nand
-  DEVICE_DTS := mt7988c-gsw-10g-sfp-spim-nand
+  DEVICE_MODEL := mt7988d-gsw-10g-sfp-spim-nand
+  DEVICE_DTS := mt7988d-gsw-10g-sfp-spim-nand
   DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
-  SUPPORTED_DEVICES := mediatek,mt7988c-gsw-10g-sfp-spim-snand
+  SUPPORTED_DEVICES := mediatek,mt7988d-gsw-10g-sfp-spim-snand
   UBINIZE_OPTS := -E 5
   BLOCKSIZE := 128k
   PAGESIZE := 2048
@@ -257,14 +257,14 @@ define Device/mediatek_mt7988c-gsw-10g-sfp-spim-nand
   IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
-TARGET_DEVICES += mediatek_mt7988c-gsw-10g-sfp-spim-nand
+TARGET_DEVICES += mediatek_mt7988d-gsw-10g-sfp-spim-nand
 
-define Device/mediatek_mt7988c-dsa-10g-spim-nand
+define Device/mediatek_mt7988d-dsa-10g-spim-nand
   DEVICE_VENDOR := MediaTek
-  DEVICE_MODEL := mt7988c-dsa-10g-spim-nand
-  DEVICE_DTS := mt7988c-dsa-10g-spim-nand
+  DEVICE_MODEL := mt7988d-dsa-10g-spim-nand
+  DEVICE_DTS := mt7988d-dsa-10g-spim-nand
   DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
-  SUPPORTED_DEVICES := mediatek,mt7988c-dsa-10g-spim-snand
+  SUPPORTED_DEVICES := mediatek,mt7988d-dsa-10g-spim-snand
   UBINIZE_OPTS := -E 5
   BLOCKSIZE := 128k
   PAGESIZE := 2048
@@ -274,14 +274,14 @@ define Device/mediatek_mt7988c-dsa-10g-spim-nand
   IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
-TARGET_DEVICES += mediatek_mt7988c-dsa-10g-spim-nand
+TARGET_DEVICES += mediatek_mt7988d-dsa-10g-spim-nand
 
-define Device/mediatek_mt7988c-dsa-e2p5g-spim-nand
+define Device/mediatek_mt7988d-dsa-e2p5g-spim-nand
   DEVICE_VENDOR := MediaTek
-  DEVICE_MODEL := mt7988c-dsa-e2p5g-spim-nand
-  DEVICE_DTS := mt7988c-dsa-e2p5g-spim-nand
+  DEVICE_MODEL := mt7988d-dsa-e2p5g-spim-nand
+  DEVICE_DTS := mt7988d-dsa-e2p5g-spim-nand
   DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
-  SUPPORTED_DEVICES := mediatek,mt7988c-dsa-e2p5g-spim-nand
+  SUPPORTED_DEVICES := mediatek,mt7988d-dsa-e2p5g-spim-nand
   UBINIZE_OPTS := -E 5
   BLOCKSIZE := 128k
   PAGESIZE := 2048
@@ -291,14 +291,14 @@ define Device/mediatek_mt7988c-dsa-e2p5g-spim-nand
   IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
-TARGET_DEVICES += mediatek_mt7988c-dsa-e2p5g-spim-nand
+TARGET_DEVICES += mediatek_mt7988d-dsa-e2p5g-spim-nand
 
-define Device/mediatek_mt7988c-dsa-10g-snfi-nand
+define Device/mediatek_mt7988d-dsa-10g-snfi-nand
   DEVICE_VENDOR := MediaTek
-  DEVICE_MODEL := mt7988c-dsa-10g-snfi-nand
-  DEVICE_DTS := mt7988c-dsa-10g-snfi-nand
+  DEVICE_MODEL := mt7988d-dsa-10g-snfi-nand
+  DEVICE_DTS := mt7988d-dsa-10g-snfi-nand
   DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
-  SUPPORTED_DEVICES := mediatek,mt7988c-dsa-10g-snfi-nand
+  SUPPORTED_DEVICES := mediatek,mt7988d-dsa-10g-snfi-nand
   UBINIZE_OPTS := -E 5
   BLOCKSIZE := 128k
   PAGESIZE := 2048
@@ -308,39 +308,39 @@ define Device/mediatek_mt7988c-dsa-10g-snfi-nand
   IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
-TARGET_DEVICES += mediatek_mt7988c-dsa-10g-snfi-nand
+TARGET_DEVICES += mediatek_mt7988d-dsa-10g-snfi-nand
 
-define Device/mediatek_mt7988c-dsa-10g-spim-nor
+define Device/mediatek_mt7988d-dsa-10g-spim-nor
   DEVICE_VENDOR := MediaTek
-  DEVICE_MODEL := mt7988c-dsa-10g-spim-nor
-  DEVICE_DTS := mt7988c-dsa-10g-spim-nor
+  DEVICE_MODEL := mt7988d-dsa-10g-spim-nor
+  DEVICE_DTS := mt7988d-dsa-10g-spim-nor
   DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
-  SUPPORTED_DEVICES := mediatek,mt7988c-dsa-10g-spim-nor
+  SUPPORTED_DEVICES := mediatek,mt7988d-dsa-10g-spim-nor
 endef
-TARGET_DEVICES += mediatek_mt7988c-dsa-10g-spim-nor
+TARGET_DEVICES += mediatek_mt7988d-dsa-10g-spim-nor
 
-define Device/mediatek_mt7988c-dsa-10g-emmc
+define Device/mediatek_mt7988d-dsa-10g-emmc
   DEVICE_VENDOR := MediaTek
-  DEVICE_MODEL := mt7988c-dsa-10g-emmc
-  DEVICE_DTS := mt7988c-dsa-10g-emmc
-  SUPPORTED_DEVICES := mediatek,mt7988c-dsa-10g-emmc
+  DEVICE_MODEL := mt7988d-dsa-10g-emmc
+  DEVICE_DTS := mt7988d-dsa-10g-emmc
+  SUPPORTED_DEVICES := mediatek,mt7988d-dsa-10g-emmc
   DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
   DEVICE_PACKAGES := mkf2fs e2fsprogs blkid blockdev losetup kmod-fs-ext4 \
 		     kmod-mmc kmod-fs-f2fs kmod-fs-vfat kmod-nls-cp437 \
 		     kmod-nls-iso8859-1
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
-TARGET_DEVICES += mediatek_mt7988c-dsa-10g-emmc
+TARGET_DEVICES += mediatek_mt7988d-dsa-10g-emmc
 
-define Device/mediatek_mt7988c-dsa-10g-sd
+define Device/mediatek_mt7988d-dsa-10g-sd
   DEVICE_VENDOR := MediaTek
-  DEVICE_MODEL := mt7988c-dsa-10g-sd
-  DEVICE_DTS := mt7988c-dsa-10g-sd
-  SUPPORTED_DEVICES := mediatek,mt7988c-dsa-10g-sd
+  DEVICE_MODEL := mt7988d-dsa-10g-sd
+  DEVICE_DTS := mt7988d-dsa-10g-sd
+  SUPPORTED_DEVICES := mediatek,mt7988d-dsa-10g-sd
   DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
   DEVICE_PACKAGES := mkf2fs e2fsprogs blkid blockdev losetup kmod-fs-ext4 \
 		     kmod-mmc kmod-fs-f2fs kmod-fs-vfat kmod-nls-cp437 \
 		     kmod-nls-iso8859-1
   IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
 endef
-TARGET_DEVICES += mediatek_mt7988c-dsa-10g-sd
+TARGET_DEVICES += mediatek_mt7988d-dsa-10g-sd
