fcf6272554479cea75b66fde4f40440dc9837f9e
developer <developer@mediatek.com>
2024-05-22 10:38:04 +0800

    [][openwrt][mt7988][hnat][Change header guard in hnat.h]
    [Description]
    Change header guard in hnat.h
    
    [Release-log]
    N/A
    
    Change-Id: I86b717fa14d79b47ce18f1a71ad1648b85defc69
    Reviewed-on: https://gerrit.mediatek.inc/c/openwrt/feeds/mtk_openwrt_feeds/+/9119756

--- 21.02/files/target/linux/mediatek/files-5.4/drivers/net/ethernet/mediatek/mtk_hnat/hnat.h
+++ 21.02/files/target/linux/mediatek/files-5.4/drivers/net/ethernet/mediatek/mtk_hnat/hnat.h
@@ -10,8 +10,8 @@
  *   Copyright (C) 2014-2016 Sean Wang <sean.wang@mediatek.com>
  *   Copyright (C) 2016-2017 John Crispin <blogic@openwrt.org>
  */
-#ifndef _MTK_HNAT_HNAT_H_
-#define _MTK_HNAT_HNAT_H_
+#ifndef NF_HNAT_H
+#define NF_HNAT_H
 
 #include <linux/debugfs.h>
 #include <linux/string.h>
@@ -1332,4 +1332,5 @@ static inline u16 foe_timestamp(struct mtk_hnat *h)
 {
 	return (readl(hnat_priv->fe_base + 0x0010)) & 0xffff;
 }
-#endif /* _MTK_HNAT_HNAT_H_ */
+
+#endif /* NF_HNAT_H */
