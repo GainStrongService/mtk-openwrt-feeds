76c9dc490b02736a77baed721d9a91be3e55c329
developer <developer@mediatek.com>
2024-09-03 16:31:54 +0800

    [][kernel][mt7988][switch][add mxl862xx SDK build]
    [Description]
    add below modification:
    1). Support mxl862xx switch on J+K SDK build
    2). fixed some build errors due to kernel compatible
    
    [Release-log]
    N/A
    
    
    Change-Id: I28e947bcf41b1c55bc874e95ccda6b1e864e6fb1
    Reviewed-on: https://gerrit.mediatek.inc/c/openwrt/feeds/mtk_openwrt_feeds/+/9588021

--- 21.02/files/target/linux/mediatek/image/mt7988.mk
+++ 21.02/files/target/linux/mediatek/image/mt7988.mk
@@ -145,6 +145,22 @@ define Device/mediatek_mt7988a-dsa-10g-spim-nand
 endef
 TARGET_DEVICES += mediatek_mt7988a-dsa-10g-spim-nand
 
+define Device/mediatek_mt7988a-dsa-mxl86252-10g-spim-nand
+  DEVICE_VENDOR := MediaTek
+  DEVICE_MODEL := mt7988a-dsa-mxl86252-10g-spim-nand
+  DEVICE_DTS := mt7988a-dsa-mxl86252-10g-spim-nand
+  DEVICE_DTS_DIR := $(DTS_DIR)/mediatek
+  SUPPORTED_DEVICES := mediatek,mt7988a-dsa-mxl86252-10g-spim-snand
+  UBINIZE_OPTS := -E 5
+  BLOCKSIZE := 128k
+  PAGESIZE := 2048
+  IMAGE_SIZE := 65536k
+  KERNEL_IN_UBI := 1
+  IMAGES += factory.bin
+  IMAGE/factory.bin := append-ubi | check-size $$$$(IMAGE_SIZE)
+  IMAGE/sysupgrade.bin := sysupgrade-tar | append-metadata
+endef
+TARGET_DEVICES += mediatek_mt7988a-dsa-mxl86252-10g-spim-nand
 
 define Device/mediatek_mt7988a-dsa-10g-spim-nand-CASAN
   DEVICE_VENDOR := MediaTek
