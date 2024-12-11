fc4fe1dceac4baaf652b7af4f5ff3c670b8578bd
developer <developer@mediatek.com>
2024-12-11 17:21:27 +0800

    [][openwrt-24][mt7988][add mt7988d-rfb support]
    [Description]
    Add mt7988d-rfb support
    
    [Release-log]
    N/A
    
    
    Change-Id: I5554e94136d5914abe6ff59ba925f931a8735155
    Reviewed-on: https://gerrit.mediatek.inc/c/openwrt/feeds/mtk_openwrt_feeds/+/10008712

---
+++ 24.10/files/target/linux/mediatek/image/filogic-extra.mk
@@ -0,0 +1,32 @@
+define Device/mediatek_mt7988d-rfb
+  DEVICE_VENDOR := MediaTek
+  DEVICE_MODEL := MT7988D rfb
+  DEVICE_DTS := mt7988d-rfb
+  DEVICE_DTS_OVERLAY:= \
+	mt7988a-rfb-emmc \
+	mt7988a-rfb-sd \
+	mt7988a-rfb-snfi-nand \
+	mt7988a-rfb-spim-nand \
+	mt7988a-rfb-spim-nand-factory \
+	mt7988a-rfb-spim-nand-nmbm \
+	mt7988a-rfb-spim-nor \
+	mt7988a-rfb-eth1-aqr \
+	mt7988a-rfb-eth1-cux3410 \
+	mt7988a-rfb-eth1-i2p5g-phy \
+	mt7988a-rfb-eth1-mxl \
+	mt7988a-rfb-eth1-sfp
+  DEVICE_DTS_DIR := $(DTS_DIR)/
+  DEVICE_DTC_FLAGS := --pad 4096
+  DEVICE_DTS_LOADADDR := 0x45f00000
+  DEVICE_PACKAGES := mt7988-2p5g-phy-firmware kmod-sfp blkid
+  KERNEL_LOADADDR := 0x46000000
+  KERNEL := kernel-bin | gzip
+  KERNEL_INITRAMFS := kernel-bin | lzma | \
+	fit lzma $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb with-initrd | pad-to 64k
+  KERNEL_INITRAMFS_SUFFIX := .itb
+  KERNEL_IN_UBI := 1
+  IMAGE_SIZE := $$(shell expr 64 + $$(CONFIG_TARGET_ROOTFS_PARTSIZE))m
+  IMAGES := sysupgrade.itb
+  IMAGE/sysupgrade.itb := append-kernel | fit gzip $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb external-with-rootfs | pad-rootfs | append-metadata
+endef
+TARGET_DEVICES += mediatek_mt7988d-rfb
