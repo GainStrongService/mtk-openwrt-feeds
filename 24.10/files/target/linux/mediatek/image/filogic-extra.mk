6511629c1233201e70369c96d457ab5cdabc94d4
developer <developer@mediatek.com>
2025-03-12 21:48:05 +0800

    [][openwrt-24][mt7988][bsp][Add SFP support for the MT7988D]
    [Description]
    Add SFP support for the MT7988D.
    
    [Release-log]
    N/A
    
    
    Change-Id: I8c3df3334210b6f81151840f2627cb9985bcdb3f
    Reviewed-on: https://gerrit.mediatek.inc/c/openwrt/feeds/mtk_openwrt_feeds/+/10374147

--- 24.10/files/target/linux/mediatek/image/filogic-extra.mk
+++ 24.10/files/target/linux/mediatek/image/filogic-extra.mk
@@ -13,7 +13,7 @@ define Device/mediatek_mt7988d-rfb
 	mt7988a-rfb-eth1-i2p5g-phy \
 	mt7988a-rfb-eth2-aqr \
 	mt7988a-rfb-eth2-mxl \
-	mt7988a-rfb-eth2-sfp \
+	mt7988d-rfb-eth2-sfp \
 	mt7988d-rfb-eth0-gsw
   DEVICE_DTS_DIR := $(DTS_DIR)/
   DEVICE_DTC_FLAGS := --pad 4096
