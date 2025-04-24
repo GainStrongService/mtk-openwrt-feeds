dcc10a8e9892ad4caee9f305a19a0a2bb3fdbce9
developer <developer@mediatek.com>
2025-04-24 15:19:54 +0800

    [][openwrt-24][common][optee][Add device node for optee]
    [Description]
    Add device node, reserve memory and firmware node, for optee
    
    [Release-log]
    
    Change-Id: Id8dd31637809d9255e195b8acbb1d549a37967e4
    Reviewed-on: https://gerrit.mediatek.inc/c/openwrt/feeds/mtk_openwrt_feeds/+/10450451

--- 24.10/files/target/linux/mediatek/image/filogic-extra.mk
+++ 24.10/files/target/linux/mediatek/image/filogic-extra.mk
@@ -28,6 +28,6 @@ define Device/mediatek_mt7988d-rfb
   KERNEL_IN_UBI := 1
   IMAGE_SIZE := $$(shell expr 64 + $$(CONFIG_TARGET_ROOTFS_PARTSIZE))m
   IMAGES := sysupgrade.itb
-  IMAGE/sysupgrade.itb := append-kernel | fit gzip $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb external-with-rootfs | pad-rootfs | append-metadata
+  IMAGE/sysupgrade.itb := append-kernel | secure-boot | fit gzip $$(KDIR)/image-$$(firstword $$(DEVICE_DTS)).dtb external-with-rootfs | pad-rootfs | append-metadata
 endef
 TARGET_DEVICES += mediatek_mt7988d-rfb
