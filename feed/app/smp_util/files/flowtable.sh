4b00b528a119cbb12e8f14e937e5ac8cd31cfee9
developer <developer@mediatek.com>
2025-05-13 10:00:01 +0800

    [][openwrt-24][MAC80211][app][Add nft flowtable rule to the smp-mt76.sh]
    [Description]
    Add nft flowtable rule to the smp-mt76.sh.
    
    Without this patch, the smp-mt76.sh script is unable to set up nft
    flowoffload rules.
    
    [Release-log]
    N/A
    
    
    Change-Id: I29cefdfebdc6a50441a0ebe544b4cef53bee6e4e
    Reviewed-on: https://gerrit.mediatek.inc/c/openwrt/feeds/mtk_openwrt_feeds/+/10633540

---
+++ feed/app/smp_util/files/flowtable.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+nftables_flowoffload_enable()
+{
+	# nft flowtable configuration file
+	flowtable_config="/etc/flowtable.conf"
+
+	# check all of the ETH/WiFi virtual interfaces
+	interfaces=$(ifconfig -a | awk -F':| ' '/^[a-zA-Z]/ {print $1}' | \
+		grep -E 'eth|lan|phy|mld|wlan|wifi|apcli|ra' | grep -v 'br-lan')
+
+	# generate ETH/WiFi virtual interfaces list
+	interfaces_list=$(echo $interfaces | tr ' ' ', ')
+
+	# generate nft flowtable configuration rules
+	cat <<EOL > $flowtable_config
+table ip filter {
+	flowtable f {
+		hook ingress priority filter + 1;
+		devices = { $interfaces_list };
+		flags offload;
+		counter;
+	}
+
+	chain forward {
+		type filter hook forward priority filter; policy accept;
+		ip protocol { tcp, udp } flow add @f;
+	}
+}
+EOL
+
+	# delete existing nft flowtable configuration
+	nft delete table ip filter
+
+	# apply nft flowtable configuration file
+	nft -f $flowtable_config
+}
+
+iptables_flowoffload_enable()
+{
+	#TCP Binding
+	iptables -D FORWARD -p tcp -m conntrack --ctstate	\
+			RELATED,ESTABLISHED -j FLOWOFFLOAD --hw
+	iptables -I FORWARD -p tcp -m conntrack --ctstate	\
+			RELATED,ESTABLISHED -j FLOWOFFLOAD --hw
+	ip6tables -D FORWARD -p tcp -m conntrack --ctstate	\
+			RELATED,ESTABLISHED -j FLOWOFFLOAD --hw
+	ip6tables -I FORWARD -p tcp -m conntrack --ctstate	\
+			RELATED,ESTABLISHED -j FLOWOFFLOAD --hw
+	#UDP Binding
+	iptables -D FORWARD -p udp -j FLOWOFFLOAD --hw
+	iptables -I FORWARD -p udp -j FLOWOFFLOAD --hw
+	ip6tables -D FORWARD -p udp -j FLOWOFFLOAD --hw
+	ip6tables -I FORWARD -p udp -j FLOWOFFLOAD --hw
+	#Multicast skip Binding
+	iptables -D FORWARD -m pkttype --pkt-type multicast -j ACCEPT
+	iptables -I FORWARD -m pkttype --pkt-type multicast -j ACCEPT
+	ip6tables -D FORWARD -m pkttype --pkt-type multicast -j ACCEPT
+	ip6tables -I FORWARD -m pkttype --pkt-type multicast -j ACCEPT
+}
--- feed/app/smp_util/files/smp-mt76.sh
+++ feed/app/smp_util/files/smp-mt76.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+source /sbin/flowtable.sh
+
 OPTIMIZED_FOR="$1"
 CPU_LIST=`cat /proc/interrupts | sed -n '1p'`
 NUM_OF_CPU=0; for i in $CPU_LIST; do NUM_OF_CPU=`expr $NUM_OF_CPU + 1`; done;
@@ -13,6 +15,7 @@ WIFI_RADIO1=0
 WIFI_RADIO2=0
 WIFI_RADIO3=0
 WED_ENABLE=0
+NFT_ENABLE=1
 
 WIFI_MODULE_LIST='mt7915e mt7996e'
 
@@ -109,23 +112,13 @@ MT7988()
 	wifi2_irq_pcie1=
 
 	if [[ "$WED_ENABLE" -eq "1" ]]; then
-		dbg2 "WED_ENABLE ON irq/iptable setting"
-		#TCP Binding
-		iptables -D FORWARD -p tcp -m conntrack --ctstate RELATED,ESTABLISHED -j FLOWOFFLOAD --hw
-		iptables -I FORWARD -p tcp -m conntrack --ctstate RELATED,ESTABLISHED -j FLOWOFFLOAD --hw
-		ip6tables -D FORWARD -p tcp -m conntrack --ctstate RELATED,ESTABLISHED -j FLOWOFFLOAD --hw
-		ip6tables -I FORWARD -p tcp -m conntrack --ctstate RELATED,ESTABLISHED -j FLOWOFFLOAD --hw
-		#UDP Binding
-		iptables -D FORWARD -p udp -j FLOWOFFLOAD --hw
-		iptables -I FORWARD -p udp -j FLOWOFFLOAD --hw
-		ip6tables -D FORWARD -p udp -j FLOWOFFLOAD --hw
-		ip6tables -I FORWARD -p udp -j FLOWOFFLOAD --hw
-		#Multicast skip Binding
-		iptables -D FORWARD -m pkttype --pkt-type multicast -j ACCEPT
-		iptables -I FORWARD -m pkttype --pkt-type multicast -j ACCEPT
-		ip6tables -D FORWARD -m pkttype --pkt-type multicast -j ACCEPT
-		ip6tables -I FORWARD -m pkttype --pkt-type multicast -j ACCEPT
-
+		if [[ "$NFT_ENABLE" -eq "1" ]]; then
+			dbg2 "WED_ENABLE ON irq/nftables setting"
+			nftables_flowoffload_enable
+		else
+			dbg2 "WED_ENABLE ON irq/iptable setting"
+			iptables_flowoffload_enable
+		fi
 	else
 		dbg2 "WED_ENABLE OFF irq/iptable seting"
 	fi
@@ -185,22 +178,13 @@ MT7986()
 	wifi3_irq=
 
 	if [[ "$WED_ENABLE" -eq "1" ]]; then
-		dbg2 "WED_ENABLE ON irq/iptable setting"
-		#TCP Binding
-		iptables -D FORWARD -p tcp -m conntrack --ctstate RELATED,ESTABLISHED -j FLOWOFFLOAD --hw
-		iptables -I FORWARD -p tcp -m conntrack --ctstate RELATED,ESTABLISHED -j FLOWOFFLOAD --hw
-		ip6tables -D FORWARD -p tcp -m conntrack --ctstate RELATED,ESTABLISHED -j FLOWOFFLOAD --hw
-		ip6tables -I FORWARD -p tcp -m conntrack --ctstate RELATED,ESTABLISHED -j FLOWOFFLOAD --hw
-		#UDP Binding
-		iptables -D FORWARD -p udp -j FLOWOFFLOAD --hw
-		iptables -I FORWARD -p udp -j FLOWOFFLOAD --hw
-		ip6tables -D FORWARD -p udp -j FLOWOFFLOAD --hw
-		ip6tables -I FORWARD -p udp -j FLOWOFFLOAD --hw
-		#Multicast skip Binding
-		iptables -D FORWARD -m pkttype --pkt-type multicast -j ACCEPT
-		iptables -I FORWARD -m pkttype --pkt-type multicast -j ACCEPT
-		ip6tables -D FORWARD -m pkttype --pkt-type multicast -j ACCEPT
-		ip6tables -I FORWARD -m pkttype --pkt-type multicast -j ACCEPT
+		if [[ "$NFT_ENABLE" -eq "1" ]]; then
+			dbg2 "WED_ENABLE ON irq/nftables setting"
+			nftables_flowoffload_enable
+		else
+			dbg2 "WED_ENABLE ON irq/iptables setting"
+			iptables_flowoffload_enable
+		fi
 
 		#AX6000 AX7800 - SOC
 		if [[ "$WIFI_RADIO1" -eq "1" ]]; then
@@ -282,26 +266,13 @@ MT7981()
 
 	#AX3000
 	if [[ "$WED_ENABLE" -eq "1" ]]; then
-		dbg2 "WED_ENABLE ON irq/iptable setting"
-		#TCP Binding
-		iptables -D FORWARD -p tcp -m conntrack --ctstate	\
-				RELATED,ESTABLISHED -j FLOWOFFLOAD --hw
-		iptables -I FORWARD -p tcp -m conntrack --ctstate	\
-				RELATED,ESTABLISHED -j FLOWOFFLOAD --hw
-		ip6tables -D FORWARD -p tcp -m conntrack --ctstate	\
-				RELATED,ESTABLISHED -j FLOWOFFLOAD --hw
-		ip6tables -I FORWARD -p tcp -m conntrack --ctstate	\
-				RELATED,ESTABLISHED -j FLOWOFFLOAD --hw
-		#UDP Binding
-		iptables -D FORWARD -p udp -j FLOWOFFLOAD --hw
-		iptables -I FORWARD -p udp -j FLOWOFFLOAD --hw
-		ip6tables -D FORWARD -p udp -j FLOWOFFLOAD --hw
-		ip6tables -I FORWARD -p udp -j FLOWOFFLOAD --hw
-		#Multicast skip Binding
-		iptables -D FORWARD -m pkttype --pkt-type multicast -j ACCEPT
-		iptables -I FORWARD -m pkttype --pkt-type multicast -j ACCEPT
-		ip6tables -D FORWARD -m pkttype --pkt-type multicast -j ACCEPT
-		ip6tables -I FORWARD -m pkttype --pkt-type multicast -j ACCEPT
+		if [[ "$NFT_ENABLE" -eq "1" ]]; then
+			dbg2 "WED_ENABLE ON irq/nftables setting"
+			nftables_flowoffload_enable
+		else
+			dbg2 "WED_ENABLE ON irq/iptable setting"
+			iptables_flowoffload_enable
+		fi
 
 		if [[ "$WIFI_RADIO1" -eq "1" ]]; then
 			wifi1_irq=237
