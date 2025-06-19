bce2f185240df28b03a8c693cd26c051a02cb8ba
developer <developer@mediatek.com>
2025-06-19 11:18:39 +0800

    [][openwrt-24][common][optee][Change using openssl3 to openssl in offline_sign.sh]
    [Description]
    Change using openssl3 to openssl in offline_sign.sh due to syncing with
    Openwrt 21.02
    
    [Release-log]
    
    Change-Id: Ie9cfe91afa5b76f447c23c25399424289cce6570
    Reviewed-on: https://gerrit.mediatek.inc/c/openwrt/feeds/mtk_openwrt_feeds/+/10801878

--- 24.10/files/package/mtk/optee-mediatek/ta-offline-sign.mk
+++ 24.10/files/package/mtk/optee-mediatek/ta-offline-sign.mk
@@ -7,7 +7,7 @@
 
 define tas-offline-sign
 	PYTHON=$(STAGING_DIR_HOSTPKG)/bin/python$(PYTHON3_VERSION) \
-	OPENSSL=$(STAGING_DIR_HOSTPKG)/bin/openssl3 \
+	OPENSSL=$(STAGING_DIR_HOST)/bin/openssl \
 	$(PKG_BUILD_DIR)/optee_apps/scripts/offline_sign.sh \
 		build_dir=$(PKG_BUILD_DIR) \
 		public_key=$(PKG_BUILD_DIR)/$(OPTEE_OS_NAME)/$(CONFIG_OPTEE_TA_PUBLIC_KEY) \
