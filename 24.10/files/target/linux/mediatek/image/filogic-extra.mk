6ed2d739afdb8bcc5da031794a217a4059f89a3f
developer <developer@mediatek.com>
2025-09-04 10:46:33 +0800

    [][Critical][kernel-6.6][mt7988/mt7987][eth][net: phy: mtk-2p5ge: Do final cleanups for kmod options & change base patch location]
    [Description]
    Add final cleanups of kmod-mt798x-2p5g-phy for mt7988d.
    Change "mtk-2p5ge driver removal patch" location from global to filogic
    to avoid openWRT-24.xx-rc patch fail.
    
    [Release-log]
    N/A
    
    Change-Id: I88aafcf5dd63cc808ce4c5cbf00987c05ef7f27f
    Reviewed-on: https://gerrit.mediatek.inc/c/openwrt/feeds/mtk_openwrt_feeds/+/11133768

--- 24.10/files/target/linux/mediatek/image/filogic-extra.mk
+++ 24.10/files/target/linux/mediatek/image/filogic-extra.mk
@@ -19,7 +19,7 @@ define Device/mediatek_mt7988d-rfb
   DEVICE_DTS_DIR := $(DTS_DIR)/
   DEVICE_DTC_FLAGS := --pad 4096
   DEVICE_DTS_LOADADDR := 0x45f00000
-  DEVICE_PACKAGES := mt798x-2p5g-phy-firmware-internal kmod-sfp blkid
+  DEVICE_PACKAGES := mt798x-2p5g-phy-firmware-internal kmod-mt798x-2p5g-phy kmod-sfp blkid
   KERNEL_LOADADDR := 0x46000000
   KERNEL := kernel-bin | gzip
   KERNEL_INITRAMFS := kernel-bin | lzma | secure-boot-initramfs | \
