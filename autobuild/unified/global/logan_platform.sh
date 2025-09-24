293e6b67326f94a0d76f914f05ed4cd599499096
developer <developer@mediatek.com>
2025-09-24 11:52:50 +0800

    [][openwrt-24][common][Add platform common OpenWrt folder for platform-logan specific patches]
    [Description]
    Add a platform common OpenWrt folder for the MT7981/MT7986/MT7987/MT7988
    platforms. This folder is intended for patches that only need to be
    applied with Logan-based drivers.
    
    [Release-log]
    N/A
    
    Change-Id: I5f6a46a5418ebad2b66077d41ae84f0fb0042344
    Reviewed-on: https://gerrit.mediatek.inc/c/openwrt/feeds/mtk_openwrt_feeds/+/11218770

---
+++ autobuild/unified/global/logan_platform.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Copyright (C) 2025 MediaTek Inc. All rights reserved.
+# Author: Sam Shih <sam.shih@mediatek.com>
+# Rules for platform-related patches and files for Logan-based WiFi builds
+
+add_patch_file_group_hooks "logan-common" "${ab_global}/logan_common"
