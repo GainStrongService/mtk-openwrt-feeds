6136d3eb0f76ece34b6f57ccf8ea3cb9383da7ae
developer <developer@mediatek.com>
2025-09-25 11:33:07 +0800

    [][openwrt-24][mt7988][autobuild][Refactor autobuild script for HQA related patches flow]
    [Description]
    Refactor the autobuild script to improve the handling flow of HQA-related
    patches.
    
    [Release-log]
    N/A
    
    Change-Id: I0b1d6a0611d4b7e82c5e7528bc33268ccb543ca8
    Reviewed-on: https://gerrit.mediatek.inc/c/openwrt/feeds/mtk_openwrt_feeds/+/11223622

---
+++ autobuild/unified/global/hqa.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# Copyright (C) 2025 MediaTek Inc. All rights reserved.
+# Author: Sam Shih <sam.shih@mediatek.com>
+# Rules for HQA related patches and files for HQA testing
+
+add_patch_file_group_hooks "hqa-common" "${ab_global}/hqa_common"
