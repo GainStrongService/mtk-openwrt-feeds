1fd355d1810c7052c26562d554101f0d096bbafe
developer <developer@mediatek.com>
2025-11-06 17:19:05 +0800

    [][openwrt-master][common][secure-boot][add anti-rollback check before flashing]
    [Description]
    Add anti-rollback check before flashing. Check the FW anti-rollback
    version for configuration nodes using the device anti-rollback version.
    The check only happens when anti-rollback option is enabled.
    
    [Release-log]
    N/A
    
    Change-Id: Ib74c79dd1ffe05b7c7d85702b81ef14f170506f0
    Reviewed-on: https://gerrit.mediatek.inc/c/openwrt/feeds/mtk_openwrt_feeds/+/11381400

--- autobuild/unified/filogic/master/files/package/mtk/secure-upgrade/files/secure_check.sh
+++ autobuild/unified/filogic/master/files/package/mtk/secure-upgrade/files/secure_check.sh
@@ -1,12 +1,19 @@
 # Script for secure image check
 # Copyright (C) 2025 MediaTek Inc.
 
+ANTI_ROLLBACK=1
+FIT_KEY="/etc/keys/fit_key.dtb"
+
 fit_verify_image()
 {
-	[ ! -f /etc/keys/fit_key.dtb ] && {
-		echo "public key does not exist"
+	local args="-f $1 -k $FIT_KEY -a"
+
+	[ ! -r "$FIT_KEY" ] && {
+		echo "public key is not readable or does not exist"
 		return 74
 	}
 
-	fit_check_sign -f "$1" -k /etc/keys/fit_key.dtb -a >/dev/null || return 74
+	[ "$ANTI_ROLLBACK" -eq 1 ] && args="$args -r"
+
+	fit_check_sign $args >/dev/null || return 74
 }
