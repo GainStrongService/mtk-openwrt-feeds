89f269152254d764d8a626a1157ed0759701bb75
developer <developer@mediatek.com>
2026-05-22 16:53:19 +0800

    [][kernel-6.6][common][eth][add license/copyright headers]
    [Description]
    Add SPDX-License-Identifier/copyright headers to ensure OSS license
    compliance.
    
    [Release-Log]
    N/A
    
    Change-Id: I29194c379cae13fe0786465cebe5c715621153fa
    Reviewed-on: https://gerrit.mediatek.inc/c/openwrt/feeds/mtk_openwrt_feeds/+/12147962

--- autobuild/unified/filogic/24.10/files/target/linux/mediatek/files-6.6/drivers/net/ethernet/mediatek/mtk_eth_dbg.c
+++ autobuild/unified/filogic/24.10/files/target/linux/mediatek/files-6.6/drivers/net/ethernet/mediatek/mtk_eth_dbg.c
@@ -1,19 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0-only
 /*
  *   Copyright (C) 2025 MediaTek Inc.
- *
- *   This program is free software; you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation; version 2 of the License
- *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
- *
- *   Copyright (C) 2009-2016 John Crispin <blogic@openwrt.org>
- *   Copyright (C) 2009-2016 Felix Fietkau <nbd@openwrt.org>
- *   Copyright (C) 2013-2016 Michael Lee <igvtee@gmail.com>
  */
+
 #include <linux/of_address.h>
 #include <linux/of_mdio.h>
 #include <linux/debugfs.h>
